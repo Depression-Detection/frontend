82126f87d3082ef6e561f0968d6d58fce5caa013
Rithwik Babu <rithwikbabu13@gmail.com>
2021-06-25 22:05:00 -0500

    Added Navbar
    (Final Build for WIP Cover page)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import { NavBar } from 'src/components/layout/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <tr>We are currently under development</tr>
+        <a href="https://twitter.com/omicron_life">Click here for Updates</a>
       </header>
     </div>
   );
