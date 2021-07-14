d7c26d0390da90cd002dfa3c9ac34867db9341e3
Rithwik Babu <rithwikbabu13@gmail.com>
2021-07-14 10:29:11 -0500

    Update App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -54,10 +54,11 @@ const AppContent = () => {
 
   return (
     <>
-      <React.Suspense fallback={<React.Fragment />}>
-        <NavBar />
-      </React.Suspense>
+      {/* <React.Suspense fallback={<React.Fragment />}>
+        
+      </React.Suspense> */}
       <React.Suspense fallback={<PageLoader />}>
+        <NavBar />
         <Switch>
           <Route exact component={ServicesPage} path="/services" />
           <Route exact component={HelpPage} path="/help" />
