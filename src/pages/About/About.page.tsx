bcef460000456ae635846beee088a022c2e0246b
rydersitcawich <rydersitcawich@gmail.com>
2021-07-29 20:50:27 -0500

    Very basic contact form on about us page

--- src/pages/About/About.page.tsx
+++ src/pages/About/About.page.tsx
@@ -135,7 +135,7 @@ export const AboutPage = () => {
       <WrapperLight>
         <Content contentCenter>
           <h2>{('Help End the Depression Epidemic')}</h2>
-          <a href="https://forms.gle/BRLPi2uAS6KPF5wg6"  target="_blank">Click here if you are interested in helping our research.</a>
+          <a href="https://forms.gle/BRLPi2uAS6KPF5wg6" target="_blank">Click here if you are interested in helping our research.</a>
         </Content>
       </WrapperLight>
       <WrapperDark>
@@ -144,6 +144,19 @@ export const AboutPage = () => {
           <ClearLink to="/about/privacy-policy">Click here to navigate to our Privacy Policy</ClearLink>
         </Content>
       </WrapperDark>
+      <WrapperLight>
+        <Content contentCenter>
+          <h2>{('Contact Us')}</h2>
+          <h3>{('Do you have any questions or concerns?')}</h3>
+          <div>
+            <form id="contact-form" method="post" action="">
+              <input name="name" type="text" placeholder="Enter Your Name" required/><br></br>
+              <input name="email" type="email" placeholder="Enter Your Email" required/><br></br>
+              <textarea name="message" placeholder="Enter Your Message"></textarea>
+          </form>
+          </div>
+        </Content>
+      </WrapperLight>
     </PageContainer>
     </>
   );
